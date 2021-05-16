b2c5bf7abbc55c47471d99ae5d0181c0ac9d8af6
Netapp DPG <netappdpg@Netapps-Air.lan>
2021-05-16 00:02:00 -0700

    fixing something

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -45,7 +45,7 @@ class SignIn extends React.Component {
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
-                <span>Sign In with you email and password</span>
+                <span>Sign In with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput 
